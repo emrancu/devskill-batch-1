d89c1721e5c7f86f6df4f4a47b90a97f8a857eea
AL EMRAN <emrancu1@gmail.com>
2023-09-22 22:04:07 +0600

    remove else and add gitignore

--- index.php
+++ index.php
@@ -23,13 +23,19 @@ function mostFavoriteSubject(array $students): string
 {
      $subjects = [];
      foreach ($students as $student){
+
          foreach ($student['subjects'] as $subject){
+
+            if(isset($subjects[$subject])){
+                 $subjects[$subject] += 1;
+             }
+
             if(!isset($subjects[$subject])){
                 $subjects[$subject] = 1;
-            }else{
-                $subjects[$subject] += 1;
             }
+
           }
+
      }
 
    $subjects = array_flip($subjects);
