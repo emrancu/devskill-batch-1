4c2d1634c881cc4c09f99566260ff0eef5b4162b
AL EMRAN <emrancu1@gmail.com>
2023-09-24 11:35:53 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,69 +1,25 @@
 <?php
 
-$students = [
-    [
-        'name' => 'John',
-        'age' => 20,
-        'subjects' => ['Math', 'Science', 'English'],
-    ],
-    [
-        'name' => 'Alice',
-        'age' => 18,
-        'subjects' => ['History', 'English'],
-    ],
-    [
-        'name' => 'Bob',
-        'age' => 19,
-        'subjects' => ['Art', 'Music'],
-    ],
-];
+class Student {
 
-function mostFavoriteSubject(array $students): string
-{
-     $subjects = [];
-     foreach ($students as $student){
-
-         foreach ($student['subjects'] as $subject){
-
-            if(isset($subjects[$subject])){
-                 $subjects[$subject] += 1;
-             }
-
-            if(!isset($subjects[$subject])){
-                $subjects[$subject] = 1;
-            }
-
-          }
-     }
-
-   $subjects = array_flip($subjects);
-
-   return end($subjects);
-}
-
-$finalResult = [];
-
-$finalResult['favorite_subject']  = mostFavoriteSubject($students);
-
-
-function array_filters(array $array, callable $function): array
-{
-  $data = [];
-
-  foreach ($array as $student){
-      if($function($student)){
-          $data[] =  $student;
-      }
-  }
-
-  return $data;
-}
-
-
-$finalResult['students_with_favourite_subject']  = array_filters($students, function ($student) use($finalResult){
-    if(in_array($finalResult['favorite_subject'], $student['subjects'])){
-      return true;
+    public function __construct(
+        public readonly string $name,
+        private  int $age,
+        public readonly array $subjects
+    ) {
     }
-});
 
-echo json_encode($finalResult);
+    public function updateAge($age): int
+    {
+       $this->age += $age;
+
+       return $this->age;
+    }
+
+}
+
+
+$student = new Student('Hasan', 23, ['English']);
+
+
+echo $student->updateAge(2);
